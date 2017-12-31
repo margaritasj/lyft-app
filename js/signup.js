fbd541a935967507a00ca97e797efb464ca46217
margb13 <marg.b13@hotmail.com>
2017-12-31 13:01:32 -0500

    Añadiendo estructura y estilos al archivo signup.html

---
+++ js/signup.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+  /* -------- Funcionalidad al ancor para retroceder a pagina anterior-------- */
+  $('#left').click(function () {
+    window.location.href = '../views/homepage.html';
+  });
+});
