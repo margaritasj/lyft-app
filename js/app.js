075401f33a7ed966ecc41079e7cdc6af7b819a95
margb13 <marg.b13@hotmail.com>
2017-12-31 10:11:01 -0500

    Añadiendo estructura y estilos al archivo index.html y funcionalidad al splash

---
+++ js/app.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  /* -------- Mostrar splash por un de tiempo de 5 seg. -------- */
+  setTimeout(function() {
+    window.location.href = '../lyft-app/views/homepage.html';
+  }, 5000);
+});
