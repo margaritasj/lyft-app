361ac9539a6752985560b721f2f104774cfa123a
margb13 <marg.b13@hotmail.com>
2017-12-31 13:22:20 -0500

    Añadiendo funcionalidad para retroceder y deshabilitando inputdel n telefonico y boton next

--- js/signup.js
+++ js/signup.js
@@ -3,4 +3,9 @@ $(document).ready(function () {
   $('#left').click(function () {
     window.location.href = '../views/homepage.html';
   });
+
+  /* -------- Deshabilitar elementos al carga la vista------- */
+  $('#num-phone').prop('disabled', true);
+  $('#btn-next').prop('disabled', true);
+
 });
