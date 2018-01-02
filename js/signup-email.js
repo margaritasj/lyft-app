15540c8a75a0713279d7b79658eec35ef9e3a043
margb13 <marg.b13@hotmail.com>
2018-01-02 10:57:38 -0500

    Validando inputs y habilitando btn-next

--- js/signup-email.js
+++ js/signup-email.js
@@ -7,9 +7,68 @@ $(document).ready(function () {
   /* -------- Deshabilitar elementos al cargar la vista------- */
   $('#btn-next').prop('disabled', true);
 
-    /* -------- Focus para el primer input al cargar la pagina------- */
-    $('#name').focus();
+  /* -------- Ocultando elemento al cargar la vista------- */
+  $('#danger').addClass('hidden');
 
+  /* -------- Focus para el primer input al cargar la pagina------- */
+  $('#name').focus();
 
+  /* -------- Validando inputs------- */
+  // Expresion regular que comprueba nombre propio y que empiecen por vocal si ésta va acentuada
+  usernameRegex = /^[A-Z a-záéíóúÁÉÍÓÚñÑ-]*$/;
+  emailRegex =  /^[a-zA-Z0-9\._-]+@[a-zA-Z0-9-]{2,}[.][a-zA-Z]{2,3}$/
 
+  function disabledButton() {
+    $('#btn-next').prop('disabled', true);
+    $('#danger').addClass('show');
+    $('#danger').removeClass('hidden');
+  }
+  
+  function disabledButton2() {
+    $('#btn-next').prop('disabled', true);
+    $('#danger').addClass('hidden');
+    $('#danger').removeClass('show')
+  }
+
+  function activeButton() {
+    $('#btn-next').prop('disabled', false);
+    $('#danger').addClass('hidden');
+    $('#danger').removeClass('show')
+  }
+
+  $('#name').keyup(function () {
+    var name = this.value;
+    // test para realizar prueba si valor del input coincide con el patron . Devuelve verdadero o falso
+    if (usernameRegex.test(name)) {
+      disabledButton2();
+      return true;
+    } else {
+      disabledButton();
+      return false;
+    }
+  });
+
+  $('#last-name').keyup(function () {
+    var lastName = this.value;
+    // test para realizar prueba si valor del input coincide con el patron . Devuelve verdadero o falso
+    if (usernameRegex.test(lastname)) {
+      disabledButton2();
+      return true;
+    } else {
+      disabledButton();
+      return false;
+    }
+  });
+
+  $('#email').keyup(function () {
+    var email = this.value;
+    // test para realizar prueba si valor del input coincide con el patron . Devuelve verdadero o falso
+    if (emailRegex.test(email)) {
+      activeButton();
+      return true;
+    } else {
+      disabledButton();
+      return false;
+    }
+  });
 });
