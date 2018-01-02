1445349e01c682e67f898f0533a59b2b15b040fc
margb13 <marg.b13@hotmail.com>
2018-01-02 11:25:32 -0500

    Añadiendo estructura de la vista check y funcionalidad al check-again

---
+++ js/check.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+  /* -------- Funcionalidad al btn sigup-------- */
+  $('#btn-signup').click(function () {
+    event.preventDefault();
+    window.location.href = '../views/signup.html';
+  });
+});
