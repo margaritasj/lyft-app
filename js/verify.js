53cb2d94be374b5b61d5eef94867cfcd78edf105
margb13 <marg.b13@hotmail.com>
2018-01-02 02:08:31 -0500

    Añadiendo validacion a los inputs cantidad de digitos y habilitar el boton next

--- js/verify.js
+++ js/verify.js
@@ -5,9 +5,55 @@ $(document).ready(function () {
   });
 
   /* -------- Deshabilitar elementos al cargar la vista------- */
+  $('#c1').focus();
   $('#c2').prop('disabled', true);
   $('#c3').prop('disabled', true);
   $('#btn-next').prop('disabled', true);
 
- 
+  /* -------- Focus para el primer input al cargar la pagina------- */
+  $('#c1').focus();
+
+  /* -------- Validando inputs con cantidad de digitos------- */
+  // Expresion regular que solo acepta numeros con una cantidad de 9 digitos
+  var pattern = /^\d{1}$/;
+
+  $('#c1').keyup(function () {
+    var firstDigit = this.value;
+
+    // test para realizar prueba si valor del input coincide con el patron . Devuelve verdadero o falso
+    if (pattern.test(firstDigit)) {
+      $('#c2').prop('disabled', false);
+      $('#c2').focus();
+      return true;
+    } else {
+      $('#btn-next').prop('disabled', true);
+      return false;
+    }
+  });
+
+  $('#c2').keyup(function () {
+    var secondDigit = this.value;
+    // test para realizar prueba si valor del input coincide con el patron . Devuelve verdadero o falso
+    if (pattern.test(secondDigit)) {
+      $('#c3').prop('disabled', false);
+      $('#c3').focus();
+      return true;
+    } else {
+      $('#btn-next').prop('disabled', true);
+      return false;
+    }
+  });
+
+  $('#c3').keyup(function () {
+    var thirdDigit = this.value;
+    // test para realizar prueba si valor del input coincide con el patron . Devuelve verdadero o falso
+    if (pattern.test(thirdDigit)) {
+      $('#btn-next').prop('disabled', false);
+      return true;
+    } else {
+      $('#btn-next').prop('disabled', true);
+      return false;
+    }
+  });
+
 });
